5b7f6b5eb37e2e4d8e862a1f5770a832a73dd5fb
Tom Walker <tpwalke2@gmail.com>
2023-08-18 07:20:06 -0400

    #6 bootstrap bluemap api connectivity; add POI markers

--- src/main/java/com/tpwalke2/bluemapsignmarkers/core/SignManager.java
+++ src/main/java/com/tpwalke2/bluemapsignmarkers/core/SignManager.java
@@ -1,5 +1,7 @@
 package com.tpwalke2.bluemapsignmarkers.core;
 
+import com.tpwalke2.bluemapsignmarkers.core.bluemap.BlueMapAPIConnector;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,11 +17,18 @@ public class SignManager {
         return INSTANCE;
     }
 
-    private SignManager() {   }
+    private final BlueMapAPIConnector blueMapAPIConnector;
+
+    private SignManager() {
+        blueMapAPIConnector = new BlueMapAPIConnector();
+    }
 
     public static void addOrUpdate(SignEntry signEntry) {
         getInstance().addOrUpdateSign(signEntry);
     }
+
+    private static final String POI_MARKER_SENTINEL = "[map]";
+
     private final ConcurrentMap<SignEntryKey, SignEntry> signCache = new ConcurrentHashMap<>();
 
     public List<SignEntry> getAllSigns() {
@@ -28,8 +37,46 @@ public class SignManager {
 
     public void addOrUpdateSign(SignEntry signEntry) {
         var key = signEntry.getKey();
+        var existing = signCache.get(key);
+        var isPOIMarker = isPOIMarker(signEntry);
 
-        signCache.put(key, signEntry);
+        // TODO add method for creating label and detail from sign text
+        var label = "";
+        var detail = "";
+
+        if (existing == null && isPOIMarker) {
+            signCache.put(key, signEntry);
+            blueMapAPIConnector.addPOIMarker(
+                    signEntry.x(),
+                    signEntry.y(),
+                    signEntry.z(),
+                    label,
+                    detail);
+            return;
+        }
+
+        if (existing != null && !isPOIMarker) {
+            removeSign(signEntry);
+            blueMapAPIConnector.removePOIMarker(
+                    signEntry.x(),
+                    signEntry.y(),
+                    signEntry.z());
+        }
+
+        if (existing != null && isPOIMarker) {
+            signCache.put(key, signEntry);
+            blueMapAPIConnector.updatePOIMarker(
+                    signEntry.x(),
+                    signEntry.y(),
+                    signEntry.z(),
+                    label,
+                    detail);
+        }
+    }
+
+    private boolean isPOIMarker(SignEntry signEntry) {
+        return signEntry.frontText().trim().startsWith(POI_MARKER_SENTINEL)
+                || signEntry.backText().trim().startsWith(POI_MARKER_SENTINEL);
     }
 
     private void removeSign(SignEntry signEntry) {
