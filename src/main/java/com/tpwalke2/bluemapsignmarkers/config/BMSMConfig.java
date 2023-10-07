0a08bcf8c9cf057f129d447739a5af813bfd93e0
Tom Walker <tpwalke2@gmail.com>
2023-10-07 07:38:37 -0400

    #37 remove remaining spongepowered references

--- src/main/java/com/tpwalke2/bluemapsignmarkers/config/BMSMConfig.java
+++ src/main/java/com/tpwalke2/bluemapsignmarkers/config/BMSMConfig.java
@@ -1,9 +1,6 @@
 package com.tpwalke2.bluemapsignmarkers.config;
 
-import org.spongepowered.configurate.objectmapping.ConfigSerializable;
-
 @SuppressWarnings({"FieldMayBeFinal", "FieldCanBeLocal"})
-@ConfigSerializable
 public class BMSMConfig {
     private String poiPrefix = "[poi]";
 
