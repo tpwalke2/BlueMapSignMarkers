be702305ee759164381cb91afad4058d2f1d24b5
MrBretze <axelolo68@gmail.com>
2024-08-24 00:27:40 +0200

    Adding new SilentLogs to remove message from the console

--- src/main/java/com/tpwalke2/bluemapsignmarkers/config/models/BMSMConfigV2.java
+++ src/main/java/com/tpwalke2/bluemapsignmarkers/config/models/BMSMConfigV2.java
@@ -4,17 +4,28 @@ import com.tpwalke2.bluemapsignmarkers.core.markers.MarkerGroup;
 import com.tpwalke2.bluemapsignmarkers.core.markers.MarkerGroupType;
 
 public final class BMSMConfigV2 {
+
+    private MarkerGroup[] markerGroups = new MarkerGroup[]{new MarkerGroup("[poi]", MarkerGroupType.POI, "Points of Interest", null, 0, 0)};
+    private boolean silentLogs = false;
+
     public BMSMConfigV2() {}
-    public BMSMConfigV2(MarkerGroup[] markerGroups) {
+
+    public BMSMConfigV2(MarkerGroup[] markerGroups, boolean silentLogs) {
         this.markerGroups = markerGroups;
+        this.silentLogs = silentLogs;
     }
     public BMSMConfigV2(MarkerGroup markerGroup) {
         this.markerGroups = new MarkerGroup[]{markerGroup};
+        this.silentLogs = false;
     }
 
-    private MarkerGroup[] markerGroups = new MarkerGroup[]{new MarkerGroup("[poi]", MarkerGroupType.POI, "Points of Interest", null, 0, 0)};
+
 
     public MarkerGroup[] getMarkerGroups() {
         return markerGroups;
+    }
+
+    public boolean isSilentLogs() {
+        return silentLogs;
     }
 }
