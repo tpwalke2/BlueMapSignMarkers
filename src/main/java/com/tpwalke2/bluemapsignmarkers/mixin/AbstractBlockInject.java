f2f2ed98aaf91a467417a6b528845a00626839c2
Tom Walker <tpwalke2@gmail.com>
2025-04-15 06:32:39 -0400

    #91 #93 AbstractBlock.onStateReplaced signature changed

--- src/main/java/com/tpwalke2/bluemapsignmarkers/mixin/AbstractBlockInject.java
+++ src/main/java/com/tpwalke2/bluemapsignmarkers/mixin/AbstractBlockInject.java
@@ -6,8 +6,8 @@ import com.tpwalke2.bluemapsignmarkers.core.signs.SignManager;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.AbstractSignBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,11 +16,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(AbstractBlock.class)
 public class AbstractBlockInject {
     @Inject(method = "onStateReplaced", at = @At("HEAD"))
-    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved, CallbackInfo ci) {
+    public void onStateReplaced(BlockState state, ServerWorld world, BlockPos pos, boolean moved, CallbackInfo ci) {
         if (world.isClient) return;
 
         if (!(state.getBlock() instanceof AbstractSignBlock)) return;
-        if (state.getBlock() == newState.getBlock()) return;
 
         SignManager.remove(new SignEntryKey(pos.getX(), pos.getY(), pos.getZ(), SignHelper.getSignParentMap(world)));
     }
